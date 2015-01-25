e706b3848551f210ca3bcbc00710ee2fdf5fa68a
Big Boss <isabella_blood@live.com>
2015-01-25 15:53:00 -0200

    Update extension.js

--- extension.js
+++ extension.js
@@ -33,14 +33,14 @@
          */
 
         bot.commands.baconCommand = {
-            command: 'pistola',  //The command to be called. With the standard command literal this would be: !bacon
+            command: 'bacon',  //The command to be called. With the standard command literal this would be: !bacon
             rank: 'user', //Minimum user permission to use the command
             type: 'exact', //Specify if it can accept variables or not (if so, these have to be handled yourself through the chat.message
             functionality: function (chat, cmd) {
                 if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
                 if (!bot.commands.executable(this.rank, chat)) return void (0);
                 else {
-                    API.sendChat("/me Tá PISTOLA!?!?!");
+                    API.sendChat("/me Bacon!!!");
                 }
             }
         };
@@ -53,12 +53,12 @@
     //Change the bots default settings and make sure they are loaded on launch
 
     localStorage.setItem("basicBotsettings", JSON.stringify({
-        botName: "Professor do Cursinho de Zoeira",
+        botName: "Cursinho de Zoeira",
         language: "english",
-        chatLink: "https://rawgit.com/B1G-B0SS/basicBot-1/master/lang/en.json",
-        maximumAfk: 60,
-        afkRemoval: false,
-        maximumDc: 30,
+        chatLink: "https://rawgit.com/B1G-B0SS/Exercito-Bot/master/lang/en.json",
+        maximumAfk: 120,
+        afkRemoval: true,
+        maximumDc: 60,
         bouncerPlus: true,
         lockdownEnabled: false,
         lockGuard: false,
@@ -66,41 +66,41 @@
         cycleGuard: true,
         maximumCycletime: 10,
         timeGuard: true,
-        maximumSongLength: 06,
-        autodisable: false,
-        commandCooldown: 20,
+        maximumSongLength: 10,
+        autodisable: true,
+        commandCooldown: 30,
         usercommandsEnabled: true,
         lockskipPosition: 3,
         lockskipReasons: [
-            ["theme", "Esse video não combina com o tema da sala. "],
+            ["theme", "This song does not fit the room theme. "],
             ["op", "This song is on the OP list. "],
-            ["history", "Esse video está no historico. "],
-            ["mix", "Você tocou um mix, é contra as regras da sala. "],
-            ["sound", "O video que você tocou tem uma qualidade ruim, ou não tem nada. "],
-            ["nsfw", "O FILHO DA PUTA, BOTA PUTARIA NA CASA DA TUA MÃE, AQUELA PUTA GORDA AFRICANA EBOLENTA, vou pular essa merda. "],
-            ["bug", "O video está bugado, irei pular. "]
+            ["history", "This song is in the history. "],
+            ["mix", "You played a mix, which is against the rules. "],
+            ["sound", "The song you played had bad sound quality or no sound. "],
+            ["nsfw", "The song you contained was NSFW (image or sound). "],
+            ["unavailable", "The song you played was not available for some users. "]
         ],
         afkpositionCheck: 15,
         afkRankCheck: "ambassador",
         motdEnabled: false,
         motdInterval: 5,
-        motd: "Delicias, lembrem de ler as regras para não levarem ban http://goo.gl/6RP5x1",
-        filterChat: false,
+        motd: "Temporary Message of the Day",
+        filterChat: true,
         etaRestriction: false,
         welcome: true,
         opLink: null,
-        rulesLink: "http://goo.gl/6RP5x1",
+        rulesLink: null,
         themeLink: null,
-        fbLink: "https://www.facebook.com/groups/zoeiraneverandsplug/",
-        youtubeLink: null
-        website: "https://8chan.co/exercito/",
+        fbLink: null,
+        youtubeLink: null,
+        website: null,
         intervalMessages: [],
         messageInterval: 5,
-        songstats: false,
+        songstats: true,
         commandLiteral: "!",
         blacklists: {
-            NSFW: "https://rawgit.com/B1G-B0SS/basicBot-customization/master/blacklists/ExampleNSFWlist.json",
-            OP: "https://rawgit.com/B1G-B0SS/basicBot-customization/master/blacklists/ExampleOPlist.json"
+            NSFW: "https://rawgit.com/Yemasthui/basicBot-customization/master/blacklists/ExampleNSFWlist.json",
+            OP: "https://rawgit.com/Yemasthui/basicBot-customization/master/blacklists/ExampleOPlist.json"
         }
     }));
 
